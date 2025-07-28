c019f267391578551eee79b7bf083d3938cc9fe1
retardedstuff <alllucknoskill_@tutanota.com>
2025-07-28 21:45:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # installs paru + packages
-pacman -Syu --noconfirm
+sudo pacman -Syu --noconfirm
 git clone https://aur.archlinux.org/paru.git
 cd paru && makepkg -si --noconfirm && cd .. && rm -rf paru
-paru --noconfirm - < packages
+paru -S --noconfirm - < packages
 
 # copy dotfiles
-cp dotfiles/home/retard/.config ~/.config
+cp -r dotfiles/home/retard/.config ~/.config
 
 # mount drives
-mkdir -p /media/Dryden
-printf "\n\n# /dev/nvme0n1p1 (Dryden)\n/dev/nvme0n1p1 /media/Dryden ext4 defaults 0 0" >> /etc/fstab
-mount -a
+sudo mkdir -p /media/Dryden
+sudo printf "\n\n# /dev/nvme0n1p1 (Dryden)\n/dev/nvme0n1p1 /media/Dryden ext4 defaults 0 0" >> /etc/fstab
+sudo mount -a
 
 # enables 'color' and 'ilovecandy' in pacman.conf
-sed -i -e 's/#Color/Color\nILoveCandy/g' /etc/pacman.conf
+sudo sed -i -e 's/#Color/Color\nILoveCandy/g' /etc/pacman.conf
 
 # inverts paru's list
-sed -i -e 's/#BottomUp/BottomUp/g' /etc/paru.conf
+sudo sed -i -e 's/#BottomUp/BottomUp/g' /etc/paru.conf
